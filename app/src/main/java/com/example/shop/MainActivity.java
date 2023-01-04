8c2bf4700480a6e83fdf473b9b081be20f321de1
Denis Kulakov <jhjxbvfhecfvf@gmail.com>
2023-01-04 09:44:54 +0400

    Initial commit

---
+++ app/src/main/java/com/example/shop/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    FragmentManager fragmentManager;
+    Fragment fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+        fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        if(fragment == null){
+            fragment = new MainFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.Computers:
+                fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+                fragment = new ComputersFragment();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .commit();
+                break;
+            case R.id.Consoles:
+                fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+                fragment = new ConsolesFragment();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .commit();
+                break;
+            case R.id.TV:
+                fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+                fragment = new TVFragment();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .commit();
+                break;
+            case R.id.Phones:
+                fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+                fragment = new PhonesFragment();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .commit();
+                break;
+            case R.id.back:
+                fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+                fragment = new MainFragment();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .commit();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/shop/PhonesFragment.java
@@ -0,0 +1,20 @@
+package com.example.shop;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PhonesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.phones_fragment, container, false);
+        return view;
+    }
+}
